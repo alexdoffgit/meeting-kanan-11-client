5447212cc3a34fe008361e191aa16b51d331b85c
alexdoffgit <alexdoffacc@gmail.com>
2022-11-15 12:38:57 +0700

    remove and modify nav

--- resources/views/layout/admin/nav.blade.php
+++ resources/views/layout/admin/nav.blade.php
@@ -11,60 +11,72 @@
             <span class="nav-link-text">Dashboard</span>
           </a>
         </li>
-		<li class="nav-item" data-toggle="tooltip" data-placement="right" title="Messages">
+		    
+        {{--
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Messages">
           <a class="nav-link" href="messages.html">
             <i class="fa fa-fw fa-envelope-open"></i>
             <span class="nav-link-text">Messages</span>
           </a>
         </li>
-		<li class="nav-item" data-toggle="tooltip" data-placement="right" title="" data-original-title="Bookings">
+		    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="" data-original-title="Bookings">
           <a class="nav-link" href="bookings.html">
             <i class="fa fa-fw fa-calendar-check-o"></i>
             <span class="nav-link-text">Bookings <span class="badge badge-pill badge-primary">6 New</span></span>
           </a>
         </li>
-		<li class="nav-item" data-toggle="tooltip" data-placement="right" title="My listings">
-          <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMylistings" data-parent="#mylistings">
+        --}}
+
+		    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="My listings">
+          <a class="nav-link{{-- nav-link-collapse collapsed --}}" {{-- data-toggle="collapse" --}} href="{{ url('/admin/listings') }}" {{-- data-parent="#mylistings" --}}>
             <i class="fa fa-fw fa-list"></i>
             <span class="nav-link-text">My listings</span>
           </a>
+          {{--
           <ul class="sidenav-second-level collapse" id="collapseMylistings">
             <li>
               <a href="{{ url('/admin/listings') }}">Pending <span class="badge badge-pill badge-primary">6</span></a>
             </li>
-			<li>
+            <li>
               <a href="{{ url('/admin/listings') }}">Active <span class="badge badge-pill badge-success">6</span></a>
             </li>
-			<li>
+            <li>
               <a href="{{ url('/admin/listings') }}">Expired <span class="badge badge-pill badge-danger">6</span></a>
             </li>
           </ul>
+          --}}
         </li>
-		<li class="nav-item" data-toggle="tooltip" data-placement="right" title="Reviews">
+
+        {{--
+		    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Reviews">
           <a class="nav-link" href="reviews.html">
             <i class="fa fa-fw fa-star"></i>
             <span class="nav-link-text">Reviews</span>
           </a>
         </li>
-		<li class="nav-item" data-toggle="tooltip" data-placement="right" title="Bookmarks">
+		    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Bookmarks">
           <a class="nav-link" href="bookmarks.html">
             <i class="fa fa-fw fa-heart"></i>
             <span class="nav-link-text">Bookmarks</span>
           </a>
         </li>
-		<li class="nav-item" data-toggle="tooltip" data-placement="right" title="Add listing">
+        --}}
+
+		    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Add listing">
           <a class="nav-link" href="{{ url('/admin/add-listing') }}">
             <i class="fa fa-fw fa-plus-circle"></i>
             <span class="nav-link-text">Add listing</span>
           </a>
         </li>
-		<li class="nav-item" data-toggle="tooltip" data-placement="right" title="My profile">
+
+        {{--
+		    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="My profile">
           <a class="nav-link" href="user-profile.html">
             <i class="fa fa-fw fa-user"></i>
             <span class="nav-link-text">My Profile</span>
           </a>
         </li>
-		<li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
+		    <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
           <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents" data-parent="#Components">
             <i class="fa fa-fw fa-gear"></i>
             <span class="nav-link-text">Components</span>
@@ -73,11 +85,13 @@
             <li>
               <a href="charts.html">Charts</a>
             </li>
-			<li>
+			      <li>
               <a href="tables.html">Tables</a>
             </li>
           </ul>
         </li>
+        --}}
+
       </ul>
       <ul class="navbar-nav sidenav-toggler">
         <li class="nav-item">
@@ -86,7 +100,9 @@
           </a>
         </li>
       </ul>
+      <!-- Top Nav Begin -->
       <ul class="navbar-nav ml-auto">
+        {{--
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle mr-lg-2" id="messagesDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <i class="fa fa-fw fa-envelope"></i>
@@ -176,10 +192,12 @@
             </div>
           </form>
         </li>
+        --}}
         <li class="nav-item">
           <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
             <i class="fa fa-fw fa-sign-out"></i>Logout</a>
         </li>
       </ul>
+      <!-- Top Nav End -->
     </div>
-  </nav>
+</nav>
