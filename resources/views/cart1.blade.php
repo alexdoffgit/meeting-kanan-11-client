dfc42b8baff44526662d39def9a70fc941009268
alexdoffgit <alexdoffacc@gmail.com>
2022-11-29 09:13:28 +0700

    fix cart layout

--- resources/views/cart1.blade.php
+++ resources/views/cart1.blade.php
@@ -88,212 +88,212 @@
 			</nav>
 	
 		</header>
-	<!-- /header -->
+		<!-- /header -->
 	
-	<main>
-		<div class="hero_in cart_section">
-			<div class="wrapper">
-				{{-- <div class="container">
-					<div class="bs-wizard clearfix">
-						<div class="bs-wizard-step active">
-							<div class="text-center bs-wizard-stepnum">Your cart</div>
-							<div class="progress">
-								<div class="progress-bar"></div>
+		<main>
+			<div class="hero_in cart_section">
+				<div class="wrapper">
+					{{-- <div class="container">
+						<div class="bs-wizard clearfix">
+							<div class="bs-wizard-step active">
+								<div class="text-center bs-wizard-stepnum">Your cart</div>
+								<div class="progress">
+									<div class="progress-bar"></div>
+								</div>
+								<a href="#0" class="bs-wizard-dot"></a>
 							</div>
-							<a href="#0" class="bs-wizard-dot"></a>
-						</div>
 
-						<div class="bs-wizard-step disabled">
-							<div class="text-center bs-wizard-stepnum">Payment</div>
-							<div class="progress">
-								<div class="progress-bar"></div>
+							<div class="bs-wizard-step disabled">
+								<div class="text-center bs-wizard-stepnum">Payment</div>
+								<div class="progress">
+									<div class="progress-bar"></div>
+								</div>
+								<a href="#0" class="bs-wizard-dot"></a>
 							</div>
-							<a href="#0" class="bs-wizard-dot"></a>
-						</div>
 
-						<div class="bs-wizard-step disabled">
-							<div class="text-center bs-wizard-stepnum">Finish!</div>
-							<div class="progress">
-								<div class="progress-bar"></div>
+							<div class="bs-wizard-step disabled">
+								<div class="text-center bs-wizard-stepnum">Finish!</div>
+								<div class="progress">
+									<div class="progress-bar"></div>
+								</div>
+								<a href="#0" class="bs-wizard-dot"></a>
 							</div>
-							<a href="#0" class="bs-wizard-dot"></a>
 						</div>
-					</div>
-					<!-- End bs-wizard -->
-				</div> --}}
+						<!-- End bs-wizard -->
+					</div> --}}
+				</div>
 			</div>
-		</div>
-		<!--/hero_in-->
+			<!--/hero_in-->
 
-		<div class="bg_color_1">
-			<div class="container margin_60_35">
-				<div class="row">
-					<div class="col-lg-8">
-						<div class="box_cart">
-						<table class="table table-striped cart-list">
-							<thead>
-								<tr>
-									<th>
-										Item
-									</th>
-									<th>
-										Discount
-									</th>
-									<th>
-										Price
-									</th>
-									<th>
-										Actions
-									</th>
-								</tr>
-							</thead>
-							<tbody>
-								@foreach ($carts['carts'] as $cart)
+			<div class="bg_color_1">
+				<div class="container margin_60_35">
+					<div class="row">
+						<div class="col-lg-8">
+							<div class="box_cart">
+							<table class="table table-striped cart-list">
+								<thead>
 									<tr>
-										<td>
-											<div class="thumb_cart">
-												<img src="{{$cart['image_icon']}}" alt="Image">
-											</div>
-											<span class="item_cart">{{$cart['room_name']}}</span>
-										</td>
-										<td>
-											0%
-										</td>
-										<td>
-											<strong>Rp.{{$cart['price']}}</strong>
-										</td>
-										<td class="options" style="width:5%; text-align:center;">
-											<a href="#0" onclick="deletecart('{{ url('/cart/delete/'.$cart['id']) }}')"><i class="icon-trash"></i></a>
-										</td>
+										<th>
+											Item
+										</th>
+										<th>
+											Discount
+										</th>
+										<th>
+											Price
+										</th>
+										<th>
+											Actions
+										</th>
 									</tr>
-								@endforeach
-							</tbody>
-						</table>
-						<div class="cart-options clearfix">
-							<div class="float-left">
-								<div class="apply-coupon">
-									<div class="form-group">
-										<input type="text" name="coupon-code" value="" placeholder="Your Coupon Code" class="form-control">
-									</div>
-									<div class="form-group">
-										<button type="button" class="btn_1 outline">Apply Coupon</button>
+								</thead>
+								<tbody>
+									@foreach ($carts['carts'] as $cart)
+										<tr>
+											<td>
+												<div class="thumb_cart">
+													<img src="{{$cart['image_icon']}}" alt="Image">
+												</div>
+												<span class="item_cart">{{$cart['room_name']}}</span>
+											</td>
+											<td>
+												0%
+											</td>
+											<td>
+												<strong>Rp.{{$cart['price']}}</strong>
+											</td>
+											<td class="options" style="width:5%; text-align:center;">
+												<a href="#0" onclick="deletecart('{{ url('/cart/delete/'.$cart['id']) }}')"><i class="icon-trash"></i></a>
+											</td>
+										</tr>
+									@endforeach
+								</tbody>
+							</table>
+							<div class="cart-options clearfix">
+								<div class="float-left">
+									<div class="apply-coupon">
+										<div class="form-group">
+											<input type="text" name="coupon-code" value="" placeholder="Your Coupon Code" class="form-control">
+										</div>
+										<div class="form-group">
+											<button type="button" class="btn_1 outline">Apply Coupon</button>
+										</div>
 									</div>
 								</div>
+								<div class="float-right fix_mobile">
+									<button type="button" class="btn_1 outline">Update Cart</button>
+								</div>
 							</div>
-							<div class="float-right fix_mobile">
-								<button type="button" class="btn_1 outline">Update Cart</button>
-							</div>
+							<!-- /cart-options -->
 						</div>
-						<!-- /cart-options -->
-					</div>
-					</div>
-					<!-- /col -->
-					
-					<aside class="col-lg-4" id="sidebar">
-						<div class="box_detail">
-							<div id="total_cart">
-								Total <span class="float-right">Rp.{{$carts['total_price']}}</span>
-							</div>
-							<ul class="cart_details">
-								<li>From <span>{{$carts['from']}}</span></li>
-								<li>To <span>{{$carts['to']}}</span></li>
-								<li>Guest <span>{{$carts['total_guest']}}</span></li>
-							</ul>
-							<button id="pay-button" class="btn_1 full-width purchase">Checkout</button>
-							<div class="text-center"><small>No money charged in this step</small></div>
 						</div>
-					</aside>
+						<!-- /col -->
+						
+						<aside class="col-lg-4" id="sidebar">
+							<div class="box_detail">
+								<div id="total_cart">
+									Total <span class="float-right">Rp.{{$carts['total_price']}}</span>
+								</div>
+								<ul class="cart_details">
+									<li>From <span>{{$carts['from']}}</span></li>
+									<li>To <span>{{$carts['to']}}</span></li>
+									<li>Guest <span>{{$carts['total_guest']}}</span></li>
+								</ul>
+								<button id="pay-button" class="btn_1 full-width purchase">Checkout</button>
+								<div class="text-center"><small>No money charged in this step</small></div>
+							</div>
+						</aside>
+					</div>
+					<!-- /row -->
 				</div>
-				<!-- /row -->
+				<!-- /container -->
 			</div>
-			<!-- /container -->
-		</div>
-		<!-- /bg_color_1 -->
-	</main>
-	<!--/main-->
-	
-	<footer>
-		<div class="container margin_60_35">
-			<div class="row">
-				<div class="col-lg-5 col-md-12 p-r-5">
-					<p><img src="img/logo.svg" width="150" height="36" alt=""></p>
-					<p>Mea nibh meis philosophia eu. Duis legimus efficiantur ea sea. Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu. Nihil facilisi indoctum an vix, ut delectus expetendis vis.</p>
-					<div class="follow_us">
-						<ul>
-							<li>Follow us</li>
-							<li><a href="#0"><i class="ti-facebook"></i></a></li>
-							<li><a href="#0"><i class="ti-twitter-alt"></i></a></li>
-							<li><a href="#0"><i class="ti-google"></i></a></li>
-							<li><a href="#0"><i class="ti-pinterest"></i></a></li>
-							<li><a href="#0"><i class="ti-instagram"></i></a></li>
+			<!-- /bg_color_1 -->
+		</main>
+		<!--/main-->
+		
+		<footer>
+			<div class="container margin_60_35">
+				<div class="row">
+					<div class="col-lg-5 col-md-12 p-r-5">
+						<p><img src="img/logo.svg" width="150" height="36" alt=""></p>
+						<p>Mea nibh meis philosophia eu. Duis legimus efficiantur ea sea. Id placerat tacimates definitionem sea, prima quidam vim no. Duo nobis persecuti cu. Nihil facilisi indoctum an vix, ut delectus expetendis vis.</p>
+						<div class="follow_us">
+							<ul>
+								<li>Follow us</li>
+								<li><a href="#0"><i class="ti-facebook"></i></a></li>
+								<li><a href="#0"><i class="ti-twitter-alt"></i></a></li>
+								<li><a href="#0"><i class="ti-google"></i></a></li>
+								<li><a href="#0"><i class="ti-pinterest"></i></a></li>
+								<li><a href="#0"><i class="ti-instagram"></i></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-6 ml-lg-auto">
+						<h5>Useful links</h5>
+						<ul class="links">
+							<li><a href="about.html">About</a></li>
+							<li><a href="login.html">Login</a></li>
+							<li><a href="register.html">Register</a></li>
+							<li><a href="blog.html">News &amp; Events</a></li>
+							<li><a href="contacts.html">Contacts</a></li>
+						</ul>
+					</div>
+					<div class="col-lg-3 col-md-6">
+						<h5>Contact with Us</h5>
+						<ul class="contacts">
+							<li><a href="tel://61280932400"><i class="ti-mobile"></i> + 61 23 8093 3400</a></li>
+							<li><a href="mailto:info@Panagea.com"><i class="ti-email"></i> info@Panagea.com</a></li>
+						</ul>
+						<div id="newsletter">
+						<h6>Newsletter</h6>
+						<div id="message-newsletter"></div>
+						<form method="post" action="assets/newsletter.php" name="newsletter_form" id="newsletter_form">
+							<div class="form-group">
+								<input type="email" name="email_newsletter" id="email_newsletter" class="form-control" placeholder="Your email">
+								<input type="submit" value="Submit" id="submit-newsletter">
+							</div>
+						</form>
+						</div>
+					</div>
+				</div>
+				<!--/row-->
+				<hr>
+				<div class="row">
+					<div class="col-lg-6">
+						<ul id="footer-selector">
+							<li>
+								<div class="styled-select" id="lang-selector">
+									<select>
+										<option value="English" selected>English</option>
+										<option value="French">French</option>
+										<option value="Spanish">Spanish</option>
+										<option value="Russian">Russian</option>
+									</select>
+								</div>
+							</li>
+							<li>
+								<div class="styled-select" id="currency-selector">
+									<select>
+										<option value="US Dollars" selected>US Dollars</option>
+										<option value="Euro">Euro</option>
+									</select>
+								</div>
+							</li>
+							<li><img src="img/cards_all.svg" alt=""></li>
+						</ul>
+					</div>
+					<div class="col-lg-6">
+						<ul id="additional_links">
+							<li><a href="#0">Terms and conditions</a></li>
+							<li><a href="#0">Privacy</a></li>
+							<li><span>© 2019 Panagea</span></li>
 						</ul>
 					</div>
 				</div>
-				<div class="col-lg-3 col-md-6 ml-lg-auto">
-					<h5>Useful links</h5>
-					<ul class="links">
-						<li><a href="about.html">About</a></li>
-						<li><a href="login.html">Login</a></li>
-						<li><a href="register.html">Register</a></li>
-						<li><a href="blog.html">News &amp; Events</a></li>
-						<li><a href="contacts.html">Contacts</a></li>
-					</ul>
-				</div>
-				<div class="col-lg-3 col-md-6">
-					<h5>Contact with Us</h5>
-					<ul class="contacts">
-						<li><a href="tel://61280932400"><i class="ti-mobile"></i> + 61 23 8093 3400</a></li>
-						<li><a href="mailto:info@Panagea.com"><i class="ti-email"></i> info@Panagea.com</a></li>
-					</ul>
-					<div id="newsletter">
-					<h6>Newsletter</h6>
-					<div id="message-newsletter"></div>
-					<form method="post" action="assets/newsletter.php" name="newsletter_form" id="newsletter_form">
-						<div class="form-group">
-							<input type="email" name="email_newsletter" id="email_newsletter" class="form-control" placeholder="Your email">
-							<input type="submit" value="Submit" id="submit-newsletter">
-						</div>
-					</form>
-					</div>
-				</div>
 			</div>
-			<!--/row-->
-			<hr>
-			<div class="row">
-				<div class="col-lg-6">
-					<ul id="footer-selector">
-						<li>
-							<div class="styled-select" id="lang-selector">
-								<select>
-									<option value="English" selected>English</option>
-									<option value="French">French</option>
-									<option value="Spanish">Spanish</option>
-									<option value="Russian">Russian</option>
-								</select>
-							</div>
-						</li>
-						<li>
-							<div class="styled-select" id="currency-selector">
-								<select>
-									<option value="US Dollars" selected>US Dollars</option>
-									<option value="Euro">Euro</option>
-								</select>
-							</div>
-						</li>
-						<li><img src="img/cards_all.svg" alt=""></li>
-					</ul>
-				</div>
-				<div class="col-lg-6">
-					<ul id="additional_links">
-						<li><a href="#0">Terms and conditions</a></li>
-						<li><a href="#0">Privacy</a></li>
-						<li><span>© 2019 Panagea</span></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</footer>
-	<!--/footer-->
+		</footer>
+		<!--/footer-->
 	</div>
 	<!-- page -->
 	
