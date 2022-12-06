d200521e123e5e660d68b748712a3477e0b70327
alexdoffgit <alexdoffacc@gmail.com>
2022-12-06 19:52:18 +0700

    change admin authenticate middleware

--- app/Http/Middleware/AdminAuthenticate.php
+++ app/Http/Middleware/AdminAuthenticate.php
@@ -16,7 +16,7 @@ class AdminAuthenticate
      */
     public function handle(Request $request, Closure $next)
     {
-        if(empty(auth()->user()) && auth()->user()->role != 'admin') {
+        if(empty(auth()->user()) || auth()->user()->role != 'admin') {
             return redirect('/admin/login');
         }
 
